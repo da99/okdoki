716894d29d3a139f5af5d741479cce96aedcf5c6
da99 <i-hate-spam-1234567@mailinator.com>
2012-11-06 09:06:10 -0600

    Updated: using new api

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,33 +1,19 @@
 
 $(function() {
 
+  var socket = io.connect('http://okdoki-disk-drive-shopper.herokuapp.com/');
 
-  // When using more than one `textarea` on your page, change the following line to match the one you’re after
-  var $textarea = $('#markdown_ex_content'),
-  $preview      = $('#html_ex');
-
-
-  var text =  "This is my list of things I hate. \n" + 
-"===================\n" +
-"* women\n* men\n* animals\n" +
-"When people read this list, they say: \n" +
-"> Are you a self-hating human?";
-
-  var converter = new Showdown.converter();
-  $preview.html(converter.makeHtml(text));
-
-  var socket = io.connect('http://localhost:4567');
-  socket.on('entering', function (data) {
-    $('#chatroom').html($('#chatroom').html() + data.html);
-    socket.emit('latest', {});
+  socket.on('connection-response', function (data) {
+    $('#chatroom').html($('#chatroom').html() + data.msg);
+    socket.emit('what-is-new?', {});
   });
 
-  socket.on('latest-response', function (data) {
-    $('#chatroom').html($('#chatroom').html() + data.html);
+  socket.on('latest', function (data) {
+    $('#chatroom').html($('#chatroom').html() + data.msg);
   });
 
-  socket.on('response-response', function (data) {
-    alert("Response: " + data.html);
+  socket.on('info-response', function (data) {
+    alert("Info: " + data.desc);
   });
 
 });
