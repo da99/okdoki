6cc7e33d7e058ee3795849ed5c6f913b2f342f9f
da99 <i-hate-spam-1234567@mailinator.com>
2012-11-17 18:48:34 -0600

    Added: started adding code for consumers.

--- migrations/chat.js
+++ migrations/chat.js
@@ -5,6 +5,21 @@ var pg = require('pg')
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query('CREATE TABLE IF NOT EXISTS bot_chat (date bigint, msg text, author varchar(20))');
-query = client.query("ALTER TABLE bot_chat ADD COLUMN id serial; ");
+query = client.query('CREATE TABLE IF NOT EXISTS bot_chat (id serial, date bigint, msg text, author varchar(20))');
+query = client.query(' \
+    CREATE TABLE IF NOT EXISTS consumers ( \
+      id varchar(15), \
+      created_at integer,  \
+      trashed_at integer,  \
+      contact_info text,   \
+      passphrase_hash,     \
+)');
+query = client.query(' \
+    CREATE TABLE IF NOT EXISTS masks ( \
+      id varchar(15) UNIQUE,   \
+      consumer_id varchar(15), \
+      created_at integer,  \
+      trashed_at integer,  \
+      name varchar(10) NOT NULL UNIQUE \
+)');
 query.on('end', function() { client.end(); });
