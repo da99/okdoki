5d97bc7ee74c4f51f18f0965e01d4def44ecda8a
da99 <i-hate-spam-1234567@mailinator.com>
2012-12-24 13:38:46 -0600

    Added: touchups

---
+++ public/assets/js/info.js
@@ -0,0 +1,18 @@
+
+$(function () {
+  $("div.screen_name div.options input[type='checkbox']").change(function () {
+    var box = $(this).closest("div.options");
+    box.addClass('loading');
+    setTimeout(function () {
+      box.removeClass('loading');
+    }, 500);
+  });
+
+  $('textarea').focus( function () {
+    var txt = $(this).val();
+    if (txt === 'Type your question here.' || txt === 'Type your reason here.') {
+      $(this).val('');
+      $(this).removeClass('blurred');
+    };
+  });
+});
