fbfc42ea2cc90ed92681ad72bd58fed2a9bb0f72
da99 <i-hate-spam-1234567@mailinator.com>
2012-12-25 14:24:10 -0600

    Added: labels to /info/:name

--- public/assets/js/info.js
+++ public/assets/js/info.js
@@ -8,6 +8,14 @@ $(function () {
     }, 500);
   });
 
+  $("div.screen_name div.options button").click(function () {
+    var box = $(this).closest("div.options");
+    box.addClass('loading');
+    setTimeout(function () {
+      box.removeClass('loading');
+    }, 500);
+  });
+
   $('textarea').focus( function () {
     var txt = $(this).val();
     if (txt === 'Type your question here.' || txt === 'Type your reason here.') {
