b2b2ecb3d835e1053f3789dd6f2256e02c01b4a3
da99 <i-hate-spam-1234567@mailinator.com>
2012-12-30 13:32:31 -0600

    Touchups to casperjs-based tests.

--- test/casper.js
+++ test/casper.js
@@ -7,6 +7,27 @@ var opts = {
     self.exit();
   }
 };
+
+function exists( s ) {
+  return function () {
+    return this.exists(s);
+  };
+};
+
+function test() {
+  var type = arguments[0];
+  var args = [];
+  var l = arguments.length;
+  var i = 1;
+  while (i < l) {
+    args.push(arguments[i]);
+    i++;
+  };
+  return function () {
+    this.test[type].apply(this.test, args);
+  };
+};
+
 var casper = require('casper').create(opts);
 var base_url = 'http://localhost:' + casper.cli.args[0];
 var phrase = 'Hoppe gives us hope';
@@ -17,6 +38,7 @@ var contact = "someone@miniuni.zbc"
 casper.start(base_url + '/', function () {
   this.test.assertHttpStatus(200);
 });
+
 
 // === Creating an account
 
@@ -41,24 +63,36 @@ casper.then(function () {
 });
 
 // === Sign-ing in
+var msg = function () {
+  return document.querySelector('#sign_in div.errors').innerHTML;
+};
 
 casper.then(function () {
   var form = 'form#form_sign_in';
   var div_errors = form + ' div.errors';
-  this.fill(form, {}, false);
 
+  // ... when no mask_name is entered.
   this.click(form + ' button.submit');
-
-  this.waitFor(function check() {
-    return this.exists(div_errors);
-  }, function then() {
-    this.test.assertExists(div_errors, div_errors + ' in form exist.');
-  },
-  null, 700);
+  var test_func = test('assertEvalEquals', msg, "Username is required.", "Msg shown: Username is required.");
+  this.waitFor( exists(div_errors), test_func, null, 700);
 
 });
 
 
+casper.then( function () {
+  var sign_in = '#sign_in form';
+  var div_errors = sign_in + ' div.errors';
 
+  // ... when no password is entered.
+  this.fill(sign_in, {
+    'mask_name': 'go99',
+    'passphrase': "",
+  }, false);
+  this.click(sign_in + ' button.submit');
+  var test_func = test( 'assertEvalEquals', msg, "Password is required.", "Msg shown: Password is required.");
+  this.waitFor(exists(div_errors), test_func, null, 700);
+});
 
-casper.run();
+casper.run(function () {
+  this.test.renderResults(true);
+});
