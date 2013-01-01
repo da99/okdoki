e52d5719e25c6e8a5f1ece03e159edb6a3fab39f
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-01 14:02:17 -0600

    Changed: Member -> Customer. (Everyone is a customer, not just a user/consumer.)

--- test/customer.js
+++ test/customer.js
@@ -1,32 +1,32 @@
 
 var _ = require('underscore');
 var assert = require('assert');
-var Member = require('okdoki/lib/Member').Member;
+var Customer = require('okdoki/lib/Customer').Customer;
 var show_databases = require('okdoki/lib/POSTGRESQL').show_databases;
 
-describe( 'Member.new', function () {
+describe( 'Customer.new', function () {
 
   it( 'checks min length of mask_name', function () {
-    var mem = new Member();
+    var mem = new Customer();
     mem.new({ password: 'something for real'});
     assert.equal(mem.errors[0].indexOf("Name must be"), 0);
   });
 
   it( 'checks max length of mask_name', function () {
-    var mem = new Member();
+    var mem = new Customer();
     mem.new({ mask_name: "123456789012345678", password: 'something for real'});
     assert.equal(mem.errors[0].indexOf("Name must be"), 0);
   });
 
 
   it( 'requires an ip address', function () {
-    var mem = new Member();
+    var mem = new Customer();
     mem.new({ mask_name: "0123456789012", password: 'something for real'});
     assert.equal(mem.errors[0].indexOf("IP address is required"), 0);
   });
 
   it( 'allows a valid mask_name', function () {
-    var mem = new Member();
+    var mem = new Customer();
     mem.new({ mask_name: "0123456", password: 'something for real', ip: '000.000.000'});
     assert.equal(mem.errors, 0);
   });
@@ -34,14 +34,14 @@ describe( 'Member.new', function () {
 }); // === describe
 
 
-describe( 'Member.new create', function () {
+describe( 'Customer.new create', function () {
 
-  it( 'saves member to db', function (done) {
-    var mem = new Member();
+  it( 'saves Customer to db', function (done) {
+    var mem = new Customer();
     var mask_name = 'mem1';
     mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.000'});
     mem.create(function () {
-      var read = new Member();
+      var read = new Customer();
       read.read(mem.customer_id, function (meta) {
         assert.equal(mem.customer_id, read.customer_id);
         done();
@@ -49,12 +49,12 @@ describe( 'Member.new create', function () {
     }, done);
   });
 
-  it( 'saves member life to db', function (done) {
-    var mem = new Member();
+  it( 'saves Customer life to db', function (done) {
+    var mem = new Customer();
     var mask_name = 'mem2';
     mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.00'});
     mem.create(function () {
-      var read = new Member();
+      var read = new Customer();
       read.read(mem.customer_id, function (meta) {
         assert.deepEqual([mask_name], read.screen_names);
         done();
@@ -63,7 +63,7 @@ describe( 'Member.new create', function () {
   });
 
   it( 'creates a database just for the new Customer', function (done) {
-    var mem = new Member();
+    var mem = new Customer();
     var mask_name = 'mem3';
     mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.00'});
     mem.create(function () {
