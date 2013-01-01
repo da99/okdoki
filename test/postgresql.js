b787122e436eb2eb14d7e0f2352075f267baa1b3
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-01 13:57:36 -0600

    Added: .on_end test for POSTGRESQL abstraction layer.

--- test/postgresql.js
+++ test/postgresql.js
@@ -5,7 +5,9 @@ var assert = require('assert');
 describe( '.on_error', function () {
 
   it( 'executes on error', function (done) {
-    var db = new pg.query('SELECT * FROM "no-table";');
+    var db = new pg.query();
+    db.q('SELECT now() AS TIME');
+    db.q('SELECT * FROM "no-table";');
     db.on_error(function (err) {
       assert.equal(err.toString(), "error: relation \"no-table\" does not exist");
       done();
@@ -13,4 +15,20 @@ describe( '.on_error', function () {
     db.run();
   });
 
+}); // === describe
+
+
+describe( '.on_end', function () {
+
+  it( 'executes on end of all queries', function (done) {
+    var db = new pg.query();
+    db.q('SELECT now() AS TIME');
+    db.q('SELECT current_database() AS NAME');
+    db.on_end(function (meta) {
+      assert.equal(meta.rows.length, 1);
+      done();
+    });
+    db.run();
+
+  });
 }); // === describe
