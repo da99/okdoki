8bc8e5e1d0eb30ccf951ca82ae47d12d03e81f2d
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-01 14:16:41 -0600

    Touchups: to long-running tests.

--- test/customer.js
+++ test/customer.js
@@ -1,8 +1,9 @@
 
-var _ = require('underscore');
-var assert = require('assert');
-var Customer = require('okdoki/lib/Customer').Customer;
-var show_databases = require('okdoki/lib/POSTGRESQL').show_databases;
+var _              = require('underscore');
+var assert         = require('assert');
+var Customer       = require('okdoki/lib/Customer').Customer;
+var pg             = require('okdoki/lib/POSTGRESQL');
+var show_databases = pg.show_databases;
 
 describe( 'Customer.new', function () {
 
@@ -36,43 +37,46 @@ describe( 'Customer.new', function () {
 
 describe( 'Customer.new create', function () {
 
-  it( 'saves Customer to db', function (done) {
+  it( 'saves Customer, Customer screen-name, and creates a Customer db', function (done) {
     var mem = new Customer();
     var mask_name = 'mem1';
     mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.000'});
     mem.create(function () {
       var read = new Customer();
       read.read(mem.customer_id, function (meta) {
+        // Has the customer id been saved?
         assert.equal(mem.customer_id, read.customer_id);
-        done();
-      });
-    }, done);
-  });
 
-  it( 'saves Customer life to db', function (done) {
-    var mem = new Customer();
-    var mask_name = 'mem2';
-    mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.00'});
-    mem.create(function () {
-      var read = new Customer();
-      read.read(mem.customer_id, function (meta) {
+        // Has the screen name been saved?
         assert.deepEqual([mask_name], read.screen_names);
-        done();
+
+        // Has the Customer's db been created?
+        show_databases(function (names) {
+          assert.equal(_.last(names), 'customer-' + read.customer_id);
+          done();
+        });
+
       });
     }, done);
   });
 
-  it( 'creates a database just for the new Customer', function (done) {
-    var mem = new Customer();
-    var mask_name = 'mem3';
-    mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.00'});
-    mem.create(function () {
-      show_databases(function (names) {
-        assert.equal(_.last(names), 'customer-' + mem.customer_id);
+}); // === describe
+
+
+describe( 'Customer.new read', function () {
+
+  it( 'reads customer from DB', function (done) {
+    var db = new pg.query('SELECT id FROM customers LIMIT 1;');
+    db.run_and_then(function (meta) {
+      var customer_id = meta.rows[0].id;
+      var mem = new Customer();
+      mem.read(customer_id, function (meta) {
+        assert(mem.customer_id, customer_id);
         done();
       });
-    }, done);
+    });
   });
+
 
 }); // === describe
 
