9b55fe6822eeb7a5344f47de0474debc91c97c4e
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-02 09:29:53 -0600

    Added: Customer update

--- test/customer.js
+++ test/customer.js
@@ -4,8 +4,22 @@ var assert         = require('assert');
 var Customer       = require('okdoki/lib/Customer').Customer;
 var pg             = require('okdoki/lib/POSTGRESQL');
 var show_databases = pg.show_databases;
+var customer_id = null;
+
+before(function (done) {
+
+  var mem = new Customer();
+  var mask_name = 'mem1';
+  mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.000'});
+  mem.create(function () {
+    customer_id = mem.customer_id;
+    done();
+  });
+
+});
 
 describe( 'Customer.new', function () {
+
 
   it( 'checks min length of mask_name', function () {
     var mem = new Customer();
@@ -35,50 +49,61 @@ describe( 'Customer.new', function () {
 }); // === describe
 
 
-describe( 'Customer.new create', function () {
+describe( 'Customer create', function () {
 
   it( 'saves Customer, Customer screen-name, and creates a Customer db', function (done) {
     var mem = new Customer();
     var mask_name = 'mem1';
-    mem.new({mask_name: mask_name, password: 'something for security', ip: '000.00.000'});
-    mem.create(function () {
-      var read = new Customer();
-      read.read(mem.customer_id, function (meta) {
-        // Has the customer id been saved?
-        assert.equal(mem.customer_id, read.customer_id);
+    var read = new Customer();
+    read.read(customer_id, function (meta) {
+      // Has the customer id been saved?
+      assert.equal(customer_id, read.customer_id);
 
-        // Has the screen name been saved?
-        assert.deepEqual([mask_name], read.screen_names);
+      // Has the screen name been saved?
+      assert.deepEqual([mask_name], read.screen_names);
 
-        // Has the Customer's db been created?
-        show_databases(function (names) {
-          assert.equal(_.last(names), 'customer-' + read.customer_id);
-          done();
-        });
-
-      });
-    }, done);
-  });
-
-}); // === describe
-
-
-describe( 'Customer.new read', function () {
-
-  it( 'reads customer from DB', function (done) {
-    var db = new pg.query('SELECT id FROM customers LIMIT 1;');
-    db.run_and_then(function (meta) {
-      var customer_id = meta.rows[0].id;
-      var mem = new Customer();
-      mem.read(customer_id, function (meta) {
-        assert(mem.customer_id, customer_id);
+      // Has the Customer's db been created?
+      show_databases(function (names) {
+        assert.equal(_.last(names), 'customer-' + read.customer_id);
         done();
       });
+
     });
   });
 
+}); // === describe
+
+describe( 'Customer read', function () {
+
+  it( 'reads customer from DB', function (done) {
+    var mem = new Customer();
+    mem.read(customer_id, function (meta) {
+      assert(mem.customer_id, customer_id);
+      done();
+    });
+  });
 
 }); // === describe
 
+
+describe( 'Customer update', function () {
+
+  it( 'updates Customer email', function (done) {
+
+    var new_email = 'new-email@i-hate-all.com';
+    var mem = new Customer(customer_id, function () {
+      mem.update({'email': new_email}, function (meta) {
+        var new_mem = new Customer(customer_id, function () {
+          new_mem.read(customer_id, function (meta) {
+            assert(new_mem.email, new_email);
+            done();
+          });
+        });
+      });
+    });
+
+  });
+
+}); // === describe
 
 
