85685a34180661a02712eec0e13a33142f800f55
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-04 11:28:42 -0600

    Refactored: merged some Screen_names logic into Customer.

--- test/customer.js
+++ test/customer.js
@@ -8,15 +8,15 @@ var strftimeUTC    = require('strftime').strftimeUTC;
 var strftime       = require('strftime').strftime;
 var show_databases = pg.show_databases;
 var customer_id    = null;
-var mask_name      = 'mem1';
+var screen_name    = 'mem1';
 
 before(function (done) {
 
-  var mask_name = 'mem1';
-  var vals = ({mask_name: mask_name, password: 'something for security', ip: '000.00.000'});
+  var screen_name = 'mem1';
+  var vals = ({screen_name: screen_name, password: 'something for security', ip: '000.00.000'});
 
   Customer.create(vals, function (mem) {
-    customer_id = mem.customer_id;
+    customer_id = mem.sanitized_data.id;
     done();
   });
 
@@ -25,27 +25,27 @@ before(function (done) {
 
 describe( 'Customer create', function () {
 
-  it( 'checks min length of mask_name', function () {
+  it( 'checks min length of screen_name', function () {
     Customer.create({ password: 'something for real'}, null, function (mem) {
       assert.equal(mem.errors[1].indexOf("Name must be"), 0);
     });
   });
 
-  it( 'checks max length of mask_name', function () {
-    Customer.create({ mask_name: "123456789012345678", password: 'something for real'}, null, function (mem) {
+  it( 'checks max length of screen_name', function () {
+    Customer.create({ screen_name: "123456789012345678", password: 'something for real'}, null, function (mem) {
       assert.equal(mem.errors[1].indexOf("Name must be"), 0);
     });
   });
 
   it( 'requires an ip address', function () {
-    Customer.create({ mask_name: "0123456789012", password: 'something for real'}, null, function (mem) {
+    Customer.create({ screen_name: "0123456789012", password: 'something for real'}, null, function (mem) {
       assert.equal(mem.errors[0].indexOf("IP address is required"), 0);
     });
   });
 
-  it( 'allows a valid mask_name', function (done) {
+  it( 'allows a valid screen_name', function (done) {
     Customer.read(customer_id, function (c, meta) {
-      assert.deepEqual(c.data.screen_names, [mask_name]);
+      assert.deepEqual(c.data.screen_names, [screen_name]);
       done();
     });
   });
@@ -54,14 +54,14 @@ describe( 'Customer create', function () {
   it( 'saves Customer, Customer screen-name, and creates a Customer db', function (done) {
     Customer.read(customer_id, function (c, meta) {
       // Has the customer id been saved?
-      assert.equal(customer_id, c.customer_id);
+      assert.equal(customer_id, c.data.id);
 
       // Has the screen name been saved?
-      assert.deepEqual([mask_name], c.data.screen_names);
+      assert.deepEqual([screen_name], c.data.screen_names);
 
       // Has the Customer's db been created?
       show_databases(function (names) {
-        assert.equal(_.last(names), 'customer-' + c.customer_id);
+        assert.equal(_.last(names), 'customer-' + c.data.id);
         done();
       });
 
@@ -74,14 +74,14 @@ describe( 'Customer read', function () {
 
   it( 'reads Customer from DB', function (done) {
     Customer.read(customer_id, function (c, meta) {
-      assert.equal(c.customer_id, customer_id);
+      assert.equal(c.data.id, customer_id);
       done();
     });
   });
 
   it( 'reads screen-names', function (done) {
     Customer.read(customer_id, function (c, meta) {
-      assert.deepEqual(c.data.screen_names, [mask_name]);
+      assert.deepEqual(c.data.screen_names, [screen_name]);
       done();
     });
   });
