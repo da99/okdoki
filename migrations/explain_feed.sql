162bf30555f3ade8521f371364adef0a49e64067
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-16 06:10:11 -0600

    Added: sql query explain

---
+++ migrations/explain_feed.sql
@@ -0,0 +1,6 @@
+﻿EXPLAIN     
+SELECT * 
+FROM posts 
+WHERE pub_id IN (SELECT pub_id FROM follows WHERE screen_name_id IN (SELECT id FROM screen_names WHERE customer_id = 'DDCcBshDT0t' ) )
+ORDER BY created_at DESC
+ ;
