8b9d770d8d0f40521e17d8ce8df0ce544cab28bc
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-22 12:34:41 -0600

    Minor refactoring.

--- test/05-casper-delete-screen-name.js
+++ test/05-casper-delete-screen-name.js
@@ -15,22 +15,24 @@ var msg = function () {
 };
 
 
-var form    = '#form_trash_screen_name ';
-var success = form + 'div.success';
-var success_msg = "Screen name, go99, has been put in trash." +
+var form          = '#form_trash_screen_name ';
+var success       = form + 'div.success';
+var success_msg   = "Screen name, go99, has been put in trash." +
   " You have 2 days from now to change your mind before it gets completely deleted.";
 var unsuccess_msg = "Screen name, go99, has been taken out of the trash.";
+var css_submit    = function (form) { return $(form + 'button.submit').css('display'); };
+var css_unsubmit  = function (form) { return $(form + 'button.unsubmit').css('display'); };
 
 casper.thenOpen(base_funcs.url + '/info/go99', function () {
-  this.test.assertEvalEquals(function (form) { return $(form + 'button.submit').css('display'); }, 'inline-block', 'Delete button shown when not trashed.', form);
-  this.test.assertEvalEquals(function (form) { return $(form + 'button.unsubmit').css('display'); }, 'none', 'Un-Delete button hidden when not trashed.', form);
+  this.test.assertEvalEquals(css_submit, 'inline-block', 'Delete button shown when not trashed.', form);
+  this.test.assertEvalEquals(css_unsubmit, 'none', 'Un-Delete button hidden when not trashed.', form);
 
   this.click(form + 'button.submit');
 
   this.waitForSelector(success, function () {
     this.test.assertEquals(this.fetchText(success), success_msg, "Msg: Time until complete deletion.");
-    this.test.assertEvalEquals(function (form) { return $(form + 'button.submit').css('display'); }, 'none', 'Delete button display=none.', form);
-    this.test.assertEvalEquals(function (form) { return $(form + 'button.unsubmit').css('display'); }, 'inline-block', 'Un-Delete button display=inline-block.', form);
+    this.test.assertEvalEquals(css_submit, 'none', 'Delete button display=none.', form);
+    this.test.assertEvalEquals(css_unsubmit, 'inline-block', 'Un-Delete button display=inline-block.', form);
   }, null, 1000);
 });
 
@@ -38,8 +40,8 @@ casper.then(function () {
   this.reload(function () {
 
     this.test.assertEquals( this.fetchText(success), success_msg, "Delete/un-delete message shown on page reload.");
-    this.test.assertEvalEquals(function (form) { return $(form + 'button.submit').css('display'); }, 'none', 'Delete button display=none.', form);
-    this.test.assertEvalEquals(function (form) { return $(form + 'button.unsubmit').css('display'); }, 'inline-block', 'Un-Delete button display=inline-block.', form);
+    this.test.assertEvalEquals(css_submit, 'none', 'Delete button display=none.', form);
+    this.test.assertEvalEquals(css_unsubmit, 'inline-block', 'Un-Delete button display=inline-block.', form);
 
   });
 });
@@ -51,8 +53,8 @@ casper.then(function () {
 
   this.waitForSelector(success, function () {
     this.test.assertEquals( this.fetchText(success), unsuccess_msg, "Undo deletion success msg shown.");
-    this.test.assertEvalEquals(function (form) { return $(form + 'button.submit').css('display'); }, 'inline-block', 'Delete button displayed.', form);
-    this.test.assertEvalEquals(function (form) { return $(form + 'button.unsubmit').css('display'); }, 'none', 'Un-Delete button hidden.', form);
+    this.test.assertEvalEquals(css_submit, 'inline-block', 'Delete button displayed.', form);
+    this.test.assertEvalEquals(css_unsubmit, 'none', 'Un-Delete button hidden.', form);
   });
 });
 
