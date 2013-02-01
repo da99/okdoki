fc3eb47a3a34720e6b858c40778a9e713da2b3af
da99 <i-hate-spam-1234567@mailinator.com>
2013-01-31 20:04:54 -0600

    Fixed: fin function can keep adding commands.

--- test/Redis.js
+++ test/Redis.js
@@ -29,7 +29,24 @@ describe( 'Redis', function () {
 
     });
 
+    R.exec();
+  });
+
+  it( 'runs commands in fin function', function (done) {
+    var  R = Redis.new();
+
+    R.add('r1', 'set', ['r1', '1']);
+
     R.exec(function () {
+      R.add('r2', 'set', ['r2', '2'], function () {
+        R.add('r3', 'set', ['r3', '3'], function () {
+          assert.equal(R.replys['r1'].reply, "OK");
+          assert.equal(R.replys['r2'].reply, "OK");
+          assert.equal(R.replys['r3'].reply, "OK");
+          done();
+        });
+      });
+
     });
   });
 
