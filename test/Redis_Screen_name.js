671c9a0d072eedbf40daeb4c1b88a04bee865e2c
da99 <i-hate-spam-1234567@mailinator.com>
2013-02-02 00:59:17 -0600

    Fixed: Redis_Screen_name create_im

---
+++ test/Redis_Screen_name.js
@@ -0,0 +1,33 @@
+var _    = require('underscore')
+, assert = require('assert')
+, RSN    = require('okdoki/lib/Redis_Screen_name').Redis_Screen_name
+, Redis  = require('okdoki/lib/Redis').Redis
+;
+
+before(function (done) {
+  RSN(Redis.client);
+  done();
+});
+
+after(function (done) {
+  Redis.client.quit();
+  done();
+});
+
+describe( 'Redis_Screen_name', function () {
+
+  describe( 'create_im', function () {
+    it( 'saves im', function (done) {
+      var body = 'Yo yo.';
+      var rsn  = RSN.new('u1');
+      rsn.create_im({body: body}, function (e, r, m) {
+        Redis.client.hget(m.id, 'body', function (e, r) {
+          assert.equal(r, body);
+          done();
+        });
+      });
+    });
+  }); // === describe
+
+}); // === describe
+
