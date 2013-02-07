7381b78af9302b6b5f6dc94cf6cfabbcb8229418
da99 <i-hate-spam-1234567@mailinator.com>
2013-02-07 14:18:50 -0600

    Fixed: updates to handle node/io/async conventions: f(err, reply)

--- test/Chat_Bot.js
+++ test/Chat_Bot.js
@@ -1,7 +1,8 @@
 
-var _      = require('underscore')
-, assert   = require('assert')
-, Chat_Bot = require('okdoki/lib/Chat_Bot').Chat_Bot
+var _         = require('underscore')
+, assert      = require('assert')
+, Redis       = require('okdoki/lib/Redis').Redis
+, Chat_Bot    = require('okdoki/lib/Chat_Bot').Chat_Bot
 , Screen_Name = require('okdoki/lib/Screen_Name').Screen_Name
 ;
 
@@ -9,15 +10,15 @@ var _      = require('underscore')
 describe( 'Chat_Bot', function () {
 
   before(function (done) {
-    Chat_Bot.redis.del('send-to-bots', done);
+    Redis.client.del('send-to-bots', done);
   });
 
   after(function (done) {
-    Chat_Bot.redis.quit(function () { done() });
+    Redis.client.quit(done);
   });
 
-  it( 'inserts a system message for unreachable bots', function (done) {
-    var r = Chat_Bot.redis.multi();
+  it( 'inserts a system message for non-existent bots', function (done) {
+    var r = Redis.client.multi();
 
     r.rpush('send-to-bots', 'MATH@GO99:1');
     r.rpush('send-to-bots', 'MONEY@DOS:2');
@@ -25,16 +26,31 @@ describe( 'Chat_Bot', function () {
     r.hmset('MONEY@DOS:2', {'from':'DOS',  to: 'MONEY@DOS', body: 'Hi 2'});
     r.exec(function (err, reply) {
       if (err) throw err;
-      Chat_Bot.deliver_ims("http://localhost:5000/bots/ims", function (mail_room) {
-        assert.equal(mail_room.ims.length, 2);
-        Screen_Name.new('GO99').read_ims_and_dims(function (msgs) {
-          assert.equal(msgs.length, 1);
-          Screen_Name.new('DOS').read_ims_and_dims(function (msgs) {
-            assert.equal(msgs.length, 1);
-            done();
-          });
+      var j = Jobs.new();
+
+      j.create('deliver ims:', "http://localhost:5000/bots/ims", function(f, g, id) {
+        Chat_Bot.deliver_ims(id, function (err, mail_room) {
+          assert.equal(mail_room.ims.length, 2);
+          f(err, mail_room);
         });
       });
+
+      j.create('read for:', 'GO99', function (f, g, id) {
+        Screen_Name.new(id).read_ims_and_dims(function (err, ims) {
+          assert.equal(ims.length, 1);
+          f(err, ims);
+        });
+      });
+
+      j.create('read for:', 'DOS', function (f, g, id) {
+        Screen_Name.new(id).read_ims_and_dims(function (err, ims) {
+          assert.equal(ims.length, 1);
+          f(err, ims);
+        });
+      });
+
+      j.run_fifo( done );
+
     });
 
   });
