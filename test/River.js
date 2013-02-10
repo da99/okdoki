2e49e54431a6e800ee55edf010c165ccfe7c5a1d
da99 <i-hate-spam-1234567@mailinator.com>
2013-02-09 20:21:15 -0600

    Fixed: tests to handle default style of: line

--- test/River.js
+++ test/River.js
@@ -7,32 +7,35 @@ var _     = require('underscore')
 
 describe( 'River', function () {
 
-  it( 'runs each job w/o waiting for the previous one to finish', function (done) {
-    var j = River.new();
-    var i = [];
+  describe( 'style: whatever', function () {
+    it( 'runs each job w/o waiting for the previous one to finish', function (done) {
+      var j = River.new();
+      j.style('whatever');
+      var i = [];
 
-    var get = function (name, r, i) {
-      r.flow.finish(null, i);
-    };
+      var get = function (name, r, i) {
+        r.flow.finish(null, i);
+      };
 
-    j.job('get:', 'google', function (r) {
-      get(r.id, r, 1);
+      j.job('get:', 'google', function (r) {
+        get(r.id, r, 1);
+      });
+
+      j.job('get:', 'bing', function (r) {
+        process.nextTick(function () { get(r.id, r, 2); });
+      });
+
+      j.job('get:', 'yahoo', function (r) {
+        get(r.id, r, 3);
+      });
+
+      j.run_and_on_finish(function () {
+        assert.deepEqual([1, 3, 2], _.flatten(j.results));
+        done();
+      });
+
     });
-
-    j.job('get:', 'bing', function (r) {
-      process.nextTick(function () { get(r.id, r, 2); });
-    });
-
-    j.job('get:', 'yahoo', function (r) {
-      get(r.id, r, 3);
-    });
-
-    j.run_and_on_finish(function () {
-      assert.deepEqual([1, 3, 2], _.flatten(j.results));
-      done();
-    });
-
-  });
+  }); // === describe
 
   describe( 'style: line', function () {
     it( 'runs job after the previous one finishes', function (done) {
@@ -62,7 +65,6 @@ describe( 'River', function () {
       });
 
       j
-      .style('line')
       .run_and_on_finish(function () {
         assert.deepEqual([null, 1, '1'], _.flatten(j.results.slice(1), 1));
         done();
