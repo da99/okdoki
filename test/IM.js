c80f9bcc47f21d654db08678468c4fc8798df39d
da99 <i-hate-spam-1234567@mailinator.com>
2013-02-21 05:21:46 -0600

    Fixed: various bugs in Contact

--- test/IM.js
+++ test/IM.js
@@ -5,6 +5,7 @@ var _      = require('underscore')
 , PG       = require('okdoki/lib/PG').PG
 , River    = require('okdoki/lib/River').River
 , Customer = require('okdoki/lib/Customer').Customer
+, Contact  = require('okdoki/lib/Contact').Contact
 , h        = require('okdoki/test/helpers')
 ;
 
@@ -14,7 +15,7 @@ describe( 'IM', function () {
   var sn_2 = 'im_auth_2';
   var sn_3 = 'im_auth_3';
 
-  var c, c2, c3;
+  var c, c2, c3, im1, im2, im3, test_river;
 
   before(function (done) {
     River.new()
@@ -22,10 +23,36 @@ describe( 'IM', function () {
     .job('create', sn,   [Customer, 'create_sample', sn])
     .job('create', sn_3, [Customer, 'create_sample', sn_3])
     .job('create', sn_2, [Customer, 'create_sample', sn_2])
+    .job('update', 'vars', function (j) {
+      var r = j.river;
+      c   = r.reply_for('create', sn);
+      c2  = r.reply_for('create', sn_2);
+      c3  = r.reply_for('create', sn_3);
+      j.finish();
+    })
+    .job('contact', 'c2->c3', function (j) {
+      Contact.create({from: c2, to_screen_name: sn_3}, j);
+    })
+    .job('contact', 'c3->c2', function (j) {
+      Contact.create({from: c3, to_screen_name: sn_2}, j);
+    })
+    .job('im', 'c1', function (j) {
+      var o = { from : sn, body : 'This is an im body.' };
+      IM.create(o, j);
+    })
+    .job('im', 'c2', function (j) {
+      var o = { from : sn_2, body : 'This is for sn_3.' };
+      IM.create(o, j);
+    })
+    .job('im', 'c3', function (j) {
+      var o = { from : sn_3, body : 'This is for sn_2.' };
+      IM.create(o, j);
+    })
     .run_and_on_finish(function (r) {
-      c  = r.reply_for('create', sn);
-      c2 = r.reply_for('create', sn_2);
-      c3 = r.reply_for('create', sn_3);
+      test_river = r;
+      im1 = r.reply_for('im', 'c1');
+      im2 = r.reply_for('im', 'c2');
+      im3 = r.reply_for('im', 'c3');
       done();
     });
   });
@@ -33,21 +60,15 @@ describe( 'IM', function () {
   describe( 'create_im:', function () {
     it( 'saves im', function (done) {
 
-      var o = {
-        from : sn,
-        body : 'This is an im body.'
-      };
-
       River.new()
-      .job('create', [IM, 'create', o])
       .job('read', function (j) {
         PG.new(j)
-        .q(SQL.select('*').from(IM.TABLE_NAME).where('id', j.river.last_reply().data.id).limit(1))
+        .q(SQL.select('*').from(IM.TABLE_NAME).where('id', im1.data.id).limit(1))
         .run()
       })
       .run_and_on_finish(function (river) {
         var r = river.last_reply();
-        assert.equal(r.body, o.body);
+        assert.equal(r.body, 'This is an im body.');
         done();
       });
 
@@ -55,20 +76,14 @@ describe( 'IM', function () {
 
     it( 'sets created_at', function (done) {
 
-      var o = {
-        from: sn,
-        body: 'something'
-      };
-
       River.new()
-      .job('create IM', [IM, 'create', o])
       .job('read', 'IM', function (j) {
         PG.new(j)
         .q(SQL.select('created_at').from(IM.TABLE_NAME)
-           .where('id', j.river.last_reply().data.id)
+           .where('id', im1.data.id)
            .limit(1)
           )
-          .run()
+        .run()
       })
       .job('read', 'now()', function (j) {
         PG.new(j)
@@ -76,9 +91,9 @@ describe( 'IM', function () {
         .run()
       })
       .run_and_on_finish(function (r) {
-        var im = r.reply_for('read', 'IM');
+        var im  = r.reply_for('read', 'IM');
         var now = r.reply_for('read', 'now()').now;
-        assert.equal((now.getTime() - im.created_at.getTime()) < 100, true);
+        assert.equal((now.getTime() - im.created_at.getTime()) < 1000, true);
         done();
       });
 
