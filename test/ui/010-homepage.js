44419bbb033a79212740ad302e7757d7b457139a
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-30 05:35:49 -0500

    Fixed: tests load after all other scripts are loaded.

--- test/ui/010-homepage.js
+++ test/ui/010-homepage.js
@@ -6,3 +6,13 @@ Akui.test("log-in form display = none", function (assert) {
 Akui.test("create account form display = none", function (assert) {
   assert('none', $('#create_account').css('display'));
 });
+
+Akui.test("Log-in form shows on click", function (assert) {
+  $('#forms a.sign_in').click();
+  assert('block', $('#sign_in').css('display'));
+});
+
+Akui.test("Create-account form shows on click", function (assert) {
+  $('#forms a.create_account').click();
+  assert('block', $('#create_account').css('display'));
+});
