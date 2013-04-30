b1824fbf7f59e5234866bdb6288cfffb3db8ba4e
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-30 11:18:06 -0500

    Refactor: bin files

--- test/ui/010-homepage.js
+++ test/ui/010-homepage.js
@@ -1,3 +1,30 @@
+// ================================================================
+// ================== Helpers =====================================
+// ================================================================
+
+function form(id) {
+  var e = $('#' + id);
+  if (!e.length)
+    throw new Error("Form #" + name + ' not found.');
+  return e;
+};
+
+function click(name) {
+  var e = $(name);
+  if (!e.length)
+    throw new Error("Link, " + name + ', not found.');
+  e.click();
+  return e;
+}
+
+function show_form(name) {
+  return click('#forms a.' + name);
+}
+
+// ================================================================
+// ================== Tests =======================================
+// ================================================================
+
 
 Akui.test("Sign-in form display = none", function (assert) {
   assert('none', $('#sign_in').css('display'));
@@ -7,24 +34,43 @@ Akui.test("create account form display = none", function (assert) {
   assert('none', $('#create_account').css('display'));
 });
 
-Akui.test("Sign-in form shows on click", function (assert) {
-  $('#forms a.sign_in').click();
+Akui.test("Sign-in form shows on show_form", function (assert) {
+  show_form('sign_in');
   assert('block', $('#sign_in').css('display'));
 });
 
-Akui.test("Create-account form shows on click", function (assert) {
-  $('#forms a.create_account').click();
+Akui.test("Create-account form shows on show_form", function (assert) {
+  show_form('create_account');
   assert('block', $('#create_account').css('display'));
 });
 
 Akui.test("Create-account form disappears when Sign-in form appears", function (assert) {
-  $('#forms a.create_account').click();
-  $('#forms a.sign_in').click();
-  assert('none', $('#create_account').css('display'));
+  show_form('create_account');
+  show_form('sign_in');
+  assert('none', form('create_account').css('display'));
 });
 
 Akui.test("Sign-in form disappears when Create-account form appears", function (assert) {
-  $('#forms a.sign_in').click();
-  $('#forms a.create_account').click();
-  assert('none', $('#sign_in').css('display'));
+  show_form('sign_in');
+  show_form('create_account');
+  assert('none', form('sign_in').css('display'));
 });
+
+// ================================================================
+// ==== Cancel buttons
+// ================================================================
+
+Akui.test('Sign-in form cancel hides form', function (assert) {
+  show_form('sign_in');
+  click('#sign_in a.cancel');
+  assert('none', form('sign_in').css('display'));
+});
+
+Akui.test('Create-account form cancel hides form', function (assert) {
+  show_form('create_account');
+  click('#create_account a.cancel');
+  assert('none', form('create_account').css('display'));
+});
+
+
+
