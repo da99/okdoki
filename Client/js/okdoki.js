60fdac4973b01adc8c159b6ab2e6a41255b7146b
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-30 07:32:02 -0500

    Fixed: bin/upgrade was ignoring _base.js

--- Client/js/okdoki.js
+++ Client/js/okdoki.js
@@ -288,3 +288,97 @@ Hyper_JS.prototype.delete_where = function (field, new_model) {
 
 
 
+
+var is_dev   = ['127.0.0.1', 'localhost'].indexOf(window.location.hostname) > -1 && window.console && window.console.log;
+var base_url = window.location.href.replace(/\/$/, '');
+var App  = _.extend({}, Backbone.Events);
+
+App.on('all', function (name) {
+  log("event: " + name);
+});
+
+function _csrf() {
+  return $.trim($('#_csrf').text());
+}
+
+function log(msg) {
+  if (is_dev)
+    return console.log(msg);
+
+  return null;
+};
+
+function func() {
+  return _.partial.apply(_, arguments);
+}
+
+function List() {}
+List.new = function (arr, ele, funcs) {
+  var a   = new List;
+  var me  = a;
+  a.list  = [];
+  a.funcs = funcs;
+  a.html  = $(ele).html();
+  a.ele   = ele;
+  $(ele).empty();
+  _.each(arr, function (piece) {
+    console.log(piece);
+    me.push(piece);
+  });
+  return a;
+};
+List.prototype.push = function (e) {
+  this.list.push(e);
+  $(this.ele).append(this.html);
+};
+
+function Listenize(html, funcs) {
+  var eles = $(html);
+
+  eles.find('input[publish-to]').each(function (i, el) {
+    var target = $(el).attr('publish-to');
+    var parent = $($(el).parents('div[id]')[0]);
+    $(el).on('keyup', function (e) {
+      parent.find(target).text($(this).val());
+    });
+  });
+
+  eles.find('button[on-click]').each(function (i, el) {
+    var func = $(el).attr('on-click');
+    console.log(func)
+    $(el).click(funcs[func]);
+    return false;
+  });
+
+  eles.find('ul[list], div[list]').each(function (i, el) {
+    var list = List.new( funcs[$(el).attr('list')], el, funcs );
+  });
+
+  return eles;
+}
+
+// ****************************************************************
+// ****************** Forms ***************************************
+// ****************************************************************
+
+function submit(e) {
+  var form = $(this).parents('form');
+  var id = $(form).attr('id');
+  try {
+  App.trigger('submit:' + id, {id: id});
+  } catch (e) {
+    log(e);
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
