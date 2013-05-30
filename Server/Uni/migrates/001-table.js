1b8f22561fc2b707c9d75fbab2823b6745e7658e
da99 <i-hate-spam-1234567@mailinator.com>
2013-05-30 03:12:11 -0500

    Better alternative to Counter.

--- Server/Uni/migrates/001-table.js
+++ Server/Uni/migrates/001-table.js
@@ -11,12 +11,13 @@ m.migrate = function (dir, r) {
   } else {
 
     var sql = 'CREATE TABLE \"' + table + "\" ( \n\
-id        serial PRIMARY KEY, \
-owner_id  int,                \
-title     text default null,  \
-about     text default null,  \
-created_at $now_tz,           \
-trashed_at $null_tz           \
+id             serial PRIMARY KEY, \
+owner_id       int,                \
+title          text default null,  \
+about          text default null,  \
+folder_seq     smallint DEFAULT 0 NOT NULL,   \
+$created_at    ,          \
+$trashed_at               \
 );";
     r.create(sql);
 
