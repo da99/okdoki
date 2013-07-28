53df6286f67d5cd3eaa1da346c2c012d839745e7
da99 <i-hate-spam-1234567@mailinator.com>
2013-07-28 12:06:17 -0500

    Touchups: model and routes templates.

--- Server/App/routes.template.js
+++ Server/App/routes.template.js
@@ -9,15 +9,8 @@ var _         = require("underscore")._
 exports.route = function (mod) {
   var app = mod.app;
 
-  // ============ READ =================================================
-  app.get("/MODEL/:id", function (req, resp, next) {
-    var OK            = mod.New_Request(arguments);
-    var opts          = OK.template_data('MODEL/show_one');
-    opts['title']     = "MODEL #" + req.params.id;
-    return OK.render_template();
-  });
-
   // ============ CREATE ===============================================
+
   app.post("/MODEL", function (req, resp, next) {
     var data = _.clone(req.body);
     mod.New_River(req, resp, next)
@@ -31,10 +24,19 @@ exports.route = function (mod) {
       resp.json({
         success : true,
         msg     : 'Created: ',
-        model   : model
+        model   : model.public_data()
       });
     })
     .run();
+  });
+
+  // ============ READ =================================================
+
+  app.get("/MODEL/:id", function (req, resp, next) {
+    var OK            = mod.New_Request(arguments);
+    var opts          = OK.template_data('MODEL/show_one');
+    opts['title']     = "MODEL #" + req.params.id;
+    return OK.render_template();
   });
 
 }; // ==== exports.routes
