6c5399624627b94b8e0bc3b35d4c81b8c0982034
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-17 04:51:03 -0500

    Fixed: path error.

--- Server/App/app.lua
+++ Server/App/app.lua
@@ -2,4 +2,4 @@
 local ut = require"pl.utils"
 
 -- ngx.say("<htm><body style=\"font-family: Ubuntu, serif; background: #fafafa;color: #EB57A3;\">Be right back.</body></html>");
-ngx.say(ut.readfile("Client/App/top_slash/markup.html"))
+ngx.say(ut.readfile("Public/App/top_slash/markup.html"))
