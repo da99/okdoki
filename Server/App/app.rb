022e58ea5c787ad929ff97dbc6b407a5a21f48ae
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-20 03:31:52 -0500

    Going back to Ruby. Don't have time to finish rack_jam

--- Server/App/app.rb
+++ Server/App/app.rb
@@ -3,5 +3,5 @@ require "sinatra"
 Temp = {:home=>nil}
 
 get "/" do
-  Temp[:home] ||= File.read("Client/App/top_slash/markup.html")
+  Temp[:home] ||= File.read("Public/App/top_slash/markup.html")
 end
