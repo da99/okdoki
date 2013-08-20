351741247ea82fb149c0ee213b944b9aa90b48cb
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-20 12:22:45 -0500

    Touchups: comments

--- Server/App/app.rb
+++ Server/App/app.rb
@@ -10,13 +10,18 @@ ContentSecurityPolicy.configure do |csp|
   csp['img-src']     =  "*"
 end
 
+# -- configure
 set :session_secret, "temp-secret-for-dev-#{Time.now}"
 enable :sessions
 
+# -- Middleware
 use Rack::Protection
 use ContentSecurityPolicy
+
+# -- Helpers
 helpers Escape_All::Sinatra
 
+# -- Routes
 get "/" do
   Fake_Mustache.new("Public/App/top_slash/markup.mustache.html", {:YEAR=>Time.now.year}).render()
 end
