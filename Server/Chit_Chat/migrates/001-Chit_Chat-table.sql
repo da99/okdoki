4486cff3ad236f7f4ad9bb9e1312309783a714bc
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-25 14:37:31 -0500

    Merged: indexes sql into table sql

--- Server/Chit_Chat/migrates/001-Chit_Chat-table.sql
+++ Server/Chit_Chat/migrates/001-Chit_Chat-table.sql
@@ -5,6 +5,7 @@ CREATE TABLE chit_chat (
   from_id           integer                  NOT NULL,
   from_type         smallint                 NOT NULL,
   to_id             integer                  NOT NULL,
+  to_type           smallint                 NOT NULL,
 
   created_at        timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
   updated_at        timestamp with time zone,
@@ -12,6 +13,8 @@ CREATE TABLE chit_chat (
 
 );
 
+CREATE INDEX chit_chat_from_to  ON  chit_chat  ( from_id,  from_type,  to_id,    to_type );
+CREATE INDEX chit_chat_to_from  ON  chit_chat  ( to_id,    to_type,    from_id,  from_type );
 
 -- DOWN
 
--- Server/Chit_Chat/migrates/002-Chit_Chat-indexes.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE INDEX chit_chat_from_to  ON  chit_chat  ( from_id,  from_type,  to_id  );
-CREATE INDEX chit_chat_to_from  ON  chit_chat  ( to_id,    from_id,    from_type );
-
--- DOWN
-
-
-
