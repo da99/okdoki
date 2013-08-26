5b0f33e72ded360b067e6809f10595f5ece6abef
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-26 13:19:28 -0500

    Touchups: to ensure session protection middleware is run after session retrieval.

--- Server/App/app.rb
+++ Server/App/app.rb
@@ -3,38 +3,16 @@ require 'content-security-policy'
 require 'sinatra'
 require 'rack/contrib'
 
-lang=ENV['LANG']
-if not lang['UTF-8']
-  raise "env $LANG is not set."
-end
-
-ss = ENV['SESSION_SECRET']
-if !ss
-  raise "No session secret set."
-end
-
-
 ContentSecurityPolicy.configure do |csp|
   csp['default-src'] =  "'self'";
   csp['img-src']     =  "*"
 end
 
-# -- configure
-use Rack::Session::Cookie, {
-  :key          => 'rack.session',
-  :path         => "/",
-  :expire_after => 60 * 60 * 24 * 7,  # 1 weeks
-  :secret       => ss,
-  :httponly     => true,
-  :secure       => true
-}
-
-
 # -- Middleware & Helpers ----------------------------
 use Rack::Protection::SessionHijacking # 0
-use ContentSecurityPolicy            # 1
-use Rack::Protection::RemoteReferrer # 2
-use Rack::PostBodyContentTypeParser  # 3
+use ContentSecurityPolicy              # 1
+use Rack::Protection::RemoteReferrer   # 2
+use Rack::PostBodyContentTypeParser    # 3
 
 # --- Order matters because they set up middelware ---
 require './Server/Ok/Escape_All'     # 1
--- conf/config.ru
+++ conf/config.ru
@@ -1,4 +1,28 @@
 
 require "./Server/App/app"
 
+lang=ENV['LANG']
+if not lang['UTF-8']
+  raise "env $LANG is not set."
+end
+
+ss = ENV['SESSION_SECRET']
+if !ss
+  raise "No session secret set."
+end
+
+# -- configure
+#    Place the session middleware
+#    first, before all other session protection
+#    middleware takes effect. See:
+#    https://github.com/sinatra/sinatra/issues/757#issuecomment-23281691
+use Rack::Session::Cookie, {
+  :key          => 'rack.session',
+  :path         => "/",
+  :expire_after => 60 * 60 * 24 * 7,  # 1 weeks
+  :secret       => ss,
+  :httponly     => true,
+  :secure       => true
+}
+
 run Sinatra::Application
