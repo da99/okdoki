7f1a2622b16a507eb9ba731b2e22f7610c3aad1c
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-26 04:11:55 -0500

    Fixed: Bot.read_by_screen_name throws better Not_Found error.

--- Server/Bot/model/read.rb
+++ Server/Bot/model/read.rb
@@ -3,7 +3,12 @@ class Bot
   class << self
 
     def read_by_screen_name sn
-      new Bot::TABLE[sn_id: sn.data[:id], sn_type: Bot::SN_TYPES::SN]
+      row = Bot::TABLE[sn_id: sn.data[:id], sn_type: Bot::SN_TYPES::SN]
+      if row
+        Bot.new(row)
+      else
+        raise self::Not_Found.new("Bot not found for: #{sn.name}")
+      end
     end
 
   end # === class self ===
--- tests/Bot/02-read.rb
+++ tests/Bot/02-read.rb
@@ -1,11 +1,41 @@
 
 require './tests/helpers'
-require './Server/Bot/model'
 
-describe "Bot: read" do
+include Screen_Name::Test
 
-  
+O = create
+S = O[:sn]
+S.create :bot
+B = Bot.read_by_screen_name(S)
 
-end # === describe Bot: read ===
+describe "Bot.read_by_screen_name" do
+
+  before do
+    @s = S
+    @b = B
+  end
+
+  it "raises Bot::Not_Found if screen name has no bot" do
+    s = create[:sn]
+    lambda {
+      Bot.read_by_screen_name s
+    }.should.raise(Bot::Not_Found)
+    .message.should.match(/Bot not found for: #{s.name}/)
+  end
+
+  it "returns a bot instance" do
+    assert :==, Bot, @b.class
+  end
+
+  it "returns a bot instance with :sn_id = data[:id]" do
+    assert :==, @s.data[:id], @b.data[:sn_id]
+  end
+
+  it "returns a bot instance with :sn_type = 0" do
+    assert 0, @b.data[:sn_type]
+  end
+
+end # === describe
+
 
 
