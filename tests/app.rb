26203a4ac13eca9113228f9e4af37abecc0ac385
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-28 05:38:31 -0500

    Refactored: server test dsl

--- tests/app.rb
+++ tests/app.rb
@@ -1,56 +1,9 @@
+require './tests/helpers'
 
-require 'pty'
-require 'httparty'
-
-def get str
-  HTTParty.get( 'https://localhost' + str )
-end
-
-def tests_started? *args
-  if args.empty?
-    @started
-  else
-    @started = args.first
-  end
-end
-
-f = nil
-
-kill = lambda { |*a|
-  old = `ps aux`['unicorn master']
-  `pkill -QUIT -f "unicorn master"`
-  f.resume if f && tests_started?
-  puts "=== Server killed\n\n" if old
-}
-
-Signal.trap("INT", kill)
-Signal.trap("EXIT", kill)
-
-kill.call
+include Server::Test
+start_server
 
 
-f = Fiber.new {
-  PTY.spawn( "bin/start" ) do |stdin, stdout, pid|
-    begin
-      # Do stuff with the output here. Just printing to show it works
-      stdin.each { |line|
-        # print line
-        Fiber.yield if line['worker'] && line['read']
-      }
-    rescue Errno::EIO
-      exit 1 if !tests_started?
-      # puts "=== Server output has ended.\n\n"
-      Fiber.yield
-    end
-  end
-}
-
-f.resume
-
-tests_started? true
-
-
-require 'Bacon_Colored'
 describe "/" do
 
   it "returns a 200" do
--- tests/helpers.rb
+++ tests/helpers.rb
@@ -1,6 +1,7 @@
 
 require './Server/Customer/model'
 require 'Bacon_Colored'
+require 'httparty'
 
 IP = '127.0.0.1'
 
@@ -108,6 +109,67 @@ def make_should o, bes_nots, meth, args
 end
 
 
+
+module Server
+  module Test
+
+    def get str
+      HTTParty.get( 'https://localhost' + str )
+    end
+
+    def tests_started? *args
+      if args.empty?
+        @started
+      else
+        @started = args.first
+      end
+    end
+
+
+    def start_server
+
+      require 'pty'
+
+      f = nil
+
+      kill = lambda { |*a|
+        old = `ps aux`['unicorn master']
+        `pkill -QUIT -f "unicorn master"`
+        f.resume if f && tests_started?
+        puts "=== Server killed\n\n" if old
+      }
+
+      Signal.trap("INT", kill)
+      Signal.trap("EXIT", kill)
+
+      kill.call
+
+
+      f = Fiber.new {
+        PTY.spawn( "bin/start" ) do |stdin, stdout, pid|
+        begin
+          # Do stuff with the output here. Just printing to show it works
+          stdin.each { |line|
+            # print line
+            Fiber.yield if line['worker'] && line['read']
+          }
+        rescue Errno::EIO
+          exit 1 if !tests_started?
+          # puts "=== Server output has ended.\n\n"
+          Fiber.yield
+        end
+        end
+      }
+
+      f.resume
+
+      tests_started? true
+
+    end # === def start_server
+
+
+  end # === module Test ===
+end # === module Server ===
 
 
 
