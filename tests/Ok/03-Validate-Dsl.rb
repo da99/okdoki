3e84693f8c14119f5b8c54f66fff1f82b1610bd7
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-28 14:17:50 -0500

    Added: tests for validate dsl: string and symbol new data keys.

---
+++ tests/Ok/03-Validate-Dsl.rb
@@ -0,0 +1,24 @@
+
+require "./tests/helpers"
+require "./Server/Ok/model"
+
+class My_Model
+  include Ok::Model
+
+  def create new_data
+    @new_data = new_data
+    validate(:hello).
+      set_to("ok dokie")
+    self
+  end
+
+end
+
+describe ":validate" do
+
+  it "can accept new_data as strings" do
+    o = My_Model.create('hello'=>'hi there')
+    o.clean_data[:hello].should == 'ok dokie'
+  end
+
+end # === describe sdff ===
