2474cf96c4b19c97ce47e82160aa1e61dc665efb
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-28 04:26:27 -0500

    Fixed: better error handling.

--- tests/app.rb
+++ tests/app.rb
@@ -18,7 +18,7 @@ kill = lambda { |*a|
 Signal.trap("INT", kill)
 Signal.trap("EXIT", kill)
 
-kill.call
+# kill.call
 
 
 require 'pty'
@@ -32,12 +32,11 @@ catch :ready do
           throw(:ready) if line['worker'] && line['read']
         }
       rescue Errno::EIO
-        puts "Errno:EIO error, but this probably just means " +
-          "that the process has finished giving output"
+        exit 1
       end
     end
   rescue PTY::ChildExited
-    puts "The child process exited!"
+    exit 1
   end
 end
 
