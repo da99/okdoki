40651cc871e650fbf9dfd6b3ba89e94e0fdb1af1
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-31 10:05:13 -0500

    Added: Bool_Params to bool-ify vals.

---
+++ Server/Ok/Bool_Params.rb
@@ -0,0 +1,30 @@
+
+module Ok
+  class Bool_Params
+
+    def initialize app, options = nil
+      @app = app
+    end
+
+    def call env
+      h = env['rack.request.form_hash']
+      if h
+        h.keys.each { |k|
+          v = h[k]
+          if k['is_']
+            h[k] = true  if v == 'true'
+            h[k] = false if v == 'false'
+          end
+        }
+      end
+      @app.call env
+    end
+
+  end # === class Bool_Params ===
+end # === module Ok ===
+
+if respond_to?(:use, true)
+
+  use Ok::Bool_Params
+
+end # === if sinatra dsl
