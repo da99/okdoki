52b593eab602386d997bbb2ade089927dfe652ce
da99 <i-hate-spam-1234567@mailinator.com>
2013-10-14 15:08:03 -0500

    Removed: publish_at  column

--- Server/Chit_Chat/migrates/001-Chit_Chat-table.sql
+++ Server/Chit_Chat/migrates/001-Chit_Chat-table.sql
@@ -7,14 +7,13 @@ CREATE TABLE chit_chat (
 
   body              text                     NOT NULL,
 
-  publish_at        timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
   created_at        timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
   updated_at        timestamp with time zone,
   trashed_at        timestamp with time zone
 
 );
 
-CREATE INDEX chit_chat_from_idx  ON  chit_chat  ( from_id, publish_at );
+CREATE INDEX chit_chat_from_idx  ON  chit_chat  ( from_id );
 
 -- DOWN
 
