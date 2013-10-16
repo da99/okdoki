5e4a5b49cb051117a099683575d7fcd45738dd76
da99 <i-hate-spam-1234567@mailinator.com>
2013-10-16 16:30:30 -0500

    Added: implementation + test of Comment.read

--- Server/Comment/model/read.rb
+++ Server/Comment/model/read.rb
@@ -1,7 +1,19 @@
 
 class Comment
 
-  
+  def id
+    data[:id]
+  end
+
+  class << self
+
+    def read pub
+      rows = Comment::TABLE.where(:pub_type_id=>to_pub_type_id(pub), :pub_id=>pub.id).all
+
+      new rows
+    end
+
+  end # === class self
 
 end # === class Comment read ===
 
--- tests/Comment/02-read.rb
+++ tests/Comment/02-read.rb
@@ -4,7 +4,20 @@ require './Server/Comment/model'
 
 describe "Comment: read" do
 
-  
+  before do
+    @sn1 = Screen_Name_Test.screen_name 0
+    id   = rand(1000)
+    @cc1 = Chit_Chat.new(:id=>id, :body=>"msg #{id}")
+  end
+
+  it "grabs all comments for a pub" do
+    c1 = Comment.create @sn1, @cc1, "msg 1"
+    c2 = Comment.create @sn1, @cc1, "msg 2"
+    c3 = Comment.create @sn1, @cc1, "msg 3"
+
+    comments = Comment.read @cc1
+    comments.map(&:id).should == [c1.id, c2.id, c3.id]
+  end
 
 end # === describe Comment: read ===
 
