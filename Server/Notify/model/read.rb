cca6728d8a874e7e940c1cef6dcad988832ec942
da99 <i-hate-spam-1234567@mailinator.com>
2013-10-19 04:09:06 -0500

    Added: Notify.read_inbox uses last_read_at for :created_at

--- Server/Notify/model/read.rb
+++ Server/Notify/model/read.rb
@@ -6,6 +6,7 @@ class Notify
     def read_inbox sn
       new TABLE
       .where(:to_id=>sn.id)
+      .and(Sequel.lit("last_read_at IS NULL OR (created_at > last_read_at)"))
       .order_by(Sequel.lit("updated_at DESC, created_at DESC"))
       .all
     end
--- tests/Notify/02-read.rb
+++ tests/Notify/02-read.rb
@@ -24,10 +24,20 @@ describe "Notify: read_inbox" do
   it "does not retrieve messages meant for others" do
     n1 = Notify.create_or_update @sn1, @sn4, "body 1"
     n2 = Notify.create_or_update @sn2, @sn4, "body 2"
-    n3 = Notify.create_or_update @sn1, @sn4, "body 3"
+    n3 = Notify.create_or_update @sn1, @sn3, "body 3"
 
     msgs = Notify.read_inbox @sn4
     msgs.map(&:id).should == [n2.id, n1.id]
+  end
+
+  it "does not retrieve messages where :created_at <= :last_read_at" do
+    n1 = Notify.create_or_update @sn1, @sn4, "body 1"
+    n2 = Notify.create_or_update @sn2, @sn4, "body 2"
+    Notify::TABLE.update(:last_read_at=>Sequel.lit("timezone('UTC'::text, now())"))
+    n3 = Notify.create_or_update @sn3, @sn4, "body 3"
+
+    msgs = Notify.read_inbox @sn4
+    msgs.map(&:id).should == [n3.id]
   end
 
 end # === describe Notify: read ===
