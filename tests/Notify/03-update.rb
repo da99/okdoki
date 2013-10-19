11cf3154ee5d79f692ce2df1e792bd265315a4d2
da99 <i-hate-spam-1234567@mailinator.com>
2013-10-19 05:08:53 -0500

    Added: implementation + test for Notify.update_last_read_at

--- tests/Notify/03-update.rb
+++ tests/Notify/03-update.rb
@@ -2,9 +2,24 @@
 require './tests/helpers'
 require './Server/Notify/model'
 
-describe "Notify: update" do
+describe "Notify: update_last_read_at" do
 
-  
+  before do
+    Notify::TABLE.delete
+    @sn1 = Screen_Name_Test.screen_name 0
+    @sn2 = Screen_Name_Test.screen_name 1
+    @sn3 = Screen_Name_Test.screen_name 2
+    @sn4 = Screen_Name_Test.screen_name 3
+  end
+
+  it "sets :last_read_at to NOW()" do
+    n1 = Notify.create_or_update @sn1, @sn4, "body 1"
+    Notify.update_last_read_at @sn1, @sn4
+    row = Notify::TABLE.first
+
+    row[:last_read_at].to_s.should
+    .match(/\d{4}-\d\d-\d\d \d\d:\d\d:\d\d -\d+/)
+  end
 
 end # === describe Notify: update ===
 
