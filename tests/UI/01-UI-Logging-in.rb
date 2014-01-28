2202d6d4307fefb28010562e29279e82a3b296c9
da99 <i-hate-spam-1234567@mailinator.com>
2014-01-28 12:03:00 -0600

    Dev. checkpoint.

--- tests/UI/01-UI-Logging-in.rb
+++ tests/UI/01-UI-Logging-in.rb
@@ -2,4 +2,6 @@
 describe "Logging in" do
 
   it "shows err msg: Too many bad log-ins for today. Try again tomorrow."
+
+  it "removes 'Create acccount' after too many failed log-ins"
 end # === desc
