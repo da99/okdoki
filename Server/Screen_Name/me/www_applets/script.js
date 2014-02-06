4a6f4ef7dc72dbe7449c9300d54337c9d82767ce
da99 <i-hate-spam-1234567@mailinator.com>
2014-02-06 00:08:39 -0600

    Refactoring: Screen_Name/me now uses new logic layout.

--- Server/Screen_Name/me/www_applets/script.js
+++ Server/Screen_Name/me/www_applets/script.js
@@ -1,31 +1,4 @@
 
-"use strict";
-
-
-
-
-
-
-// ================================================================
-// Load chit-chat data
-// ================================================================
-
-$(function () {
-
-  post('/Chit_Chat/list', {screen_name: Template.read_content('div.screen_name')}, function (err, result) {
-    if (err) {
-      log('Error in /Chit_Chat/list: ', err, result);
-      return;
-    }
-    if (_.isEmpty(result.chit_chat_list)) {
-      var empty = Template.read('div.empty.chit_chat_list');
-      var b = $('#Message_Board div.content');
-      b.find('div.please_wait.loading').replaceWith(empty);
-    }
-  });
-
-}); // ==== jquery on dom ready
-
 
 // ================================================================
 // ================== WWW_Applet ==================================
