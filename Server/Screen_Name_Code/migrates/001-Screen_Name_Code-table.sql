d82e5e1561abeeab40200a96c540ddf1cb2495a4
da99 <i-hate-spam-1234567@mailinator.com>
2014-02-22 02:34:44 -0600

    Fixed: column of wrong type

--- Server/Screen_Name_Code/migrates/001-Screen_Name_Code-table.sql
+++ Server/Screen_Name_Code/migrates/001-Screen_Name_Code-table.sql
@@ -2,10 +2,10 @@
 CREATE TABLE screen_name_code (
   id             serial   PRIMARY KEY,
   screen_name_id integer                  NOT NULL,
-  event_name_id  character varying(100)   NOT NULL,
-  code        text                        NOT NULL,
-  created_at  timestamp with time zone    NOT NULL DEFAULT timezone('UTC'::text, now()),
-  updated_at  timestamp with time zone
+  event_name_id  smallint                 NOT NULL,
+  code           text                     NOT NULL,
+  created_at     timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
+  updated_at     timestamp with time zone
 );
 
 CREATE UNIQUE INDEX unique_screen_name_id_to_event_name_id_idx ON screen_name_code (screen_name_id, event_name_id);
