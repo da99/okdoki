29497ca21fa43fa17f77deb8d076a0e45a4a0556
da99 <i-hate-spam-1234567@mailinator.com>
2014-04-09 12:19:38 -0500

    Fixed: Code fields and unique index.

--- Server/Code/migrates/001-Code-table.sql
+++ Server/Code/migrates/001-Code-table.sql
@@ -1,16 +1,16 @@
 
 CREATE TABLE code (
-  id             serial   PRIMARY KEY,
-  is_on          boolean                  NOT NULL DEFAULT false,
-  screen_name_id integer                  NOT NULL,
-  event_name_id  smallint                 NOT NULL,
+  id             SERIAL                                    PRIMARY KEY,
+  class_id       smallint                 NOT NULL,
+  producer_id    integer                  NOT NULL,
+  producer_class_id smallint              NOT NULL,
   code           text                     DEFAULT NULL,
   ss_code        text                     DEFAULT NULL,
   created_at     timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
   updated_at     timestamp with time zone
 );
 
-CREATE UNIQUE INDEX unique_screen_name_id_to_event_name_id_idx ON code (screen_name_id, event_name_id);
+CREATE UNIQUE INDEX unique_code_idx ON code (producer_id, producer_class_id, class_id);
 
 -- DOWN
 
