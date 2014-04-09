b6f996c3ff0e7cd95141436351de4cdfdb1bedc2
da99 <i-hate-spam-1234567@mailinator.com>
2014-04-09 12:27:20 -0500

    Fixed: Code has an author (producer) and a target (consumer)

--- Server/Code/migrates/001-Code-table.sql
+++ Server/Code/migrates/001-Code-table.sql
@@ -2,9 +2,15 @@
 CREATE TABLE code (
 
   id             SERIAL                                    PRIMARY KEY,
+
   class_id       smallint                 NOT NULL,
-  producer_id    integer                  NOT NULL,
+
+  producer_id       integer               NOT NULL,
   producer_class_id smallint              NOT NULL,
+
+  consumer_id       integer               NOT NULL,
+  consumer_class_id smallint              NOT NULL,
+
   code           text                     DEFAULT NULL,
   ss_code        text                     DEFAULT NULL,
   created_at     timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
