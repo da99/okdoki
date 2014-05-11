7929623c1d06f9d0c19684c2be1cce828f331668
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-11 08:47:31 -0500

    Dev. checkpoint.

---
+++ specs/intro/Consume-Message-List.rb
@@ -0,0 +1,9 @@
+
+
+describe "Consuming a message list:" do
+
+  it "retrieves messages from a consume list"
+  it "does not include messages from a life that has banned the Consumer"
+  it "does not include messages if life is protected and Consumer is not allowed."
+
+end # === desc
