537c7ee128521f185ad4e741641b0fa86a24de6a
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-13 16:28:01 -0500

    Dev. checkpoint.

--- specs/intro/Consume-Message-List.rb
+++ specs/intro/Consume-Message-List.rb
@@ -6,26 +6,6 @@ describe "Consuming a message list:" do
     delete_all Screen_Name, Customer
   end
 
-  it "does not include messages from a life that has banned the Consumer"
-  it "does not include messages if life is protected and Consumer is not allowed."
-
-  it "does not retreive messages if consumer is banned by producer" do
-    consumer = customer_create 'bob'
-
-    producer_1 = customer_create "star_1"
-    Message.create producer_1, "Prod 1"
-
-    producer_2 = customer_create "star_2"
-    Message.create producer_1, "Prod 2"
-    
-    Consume.create 'follow', consumer, producer_1
-    Consume.create 'follow', consumer, producer_2
-    Consume.create 'ban', consumer, producer_2
-
-    list = Message.read_list 'follow', consumer
-    list.map { |m| m.data.body }.should == ["Prod _1"]
-  end
-
   it "retrieves the latest 25 messages from a consume list" do
     # === Create test data
     consumer = customer_create 'bob'
@@ -35,9 +15,9 @@ describe "Consuming a message list:" do
       producers << customer_create "star_#{num}"
       10.times do |num|
         msgs.unshift "Hello msg #{producers.last.screen_name}: #{num}"
-        Message.create producers.last, msgs.first
+        Message.create producers.last.screen_name, msgs.first
       end
-      Consume.create 'follow',  consumer, producers.last
+      Consume.create 'follow',  consumer.screen_name, producers.last.screen_name
     end
 
     # === test it
@@ -46,5 +26,39 @@ describe "Consuming a message list:" do
     list.first.data.body.should == msgs.first
     list.last.data.body.should  == msgs[24]
   end # === it
+
+  it "does not include messages if producer life is protected and Consumer is not allowed." do
+    consumer = customer_create 'bob'
+    producer_1 = customer_create 'star_1'
+    producer_2 = customer_create 'star_2'
+
+    Message.create producer_1.screen_name, 'Msg 1'
+    Message.create producer_2.screen_name, 'MSG 2', :to, [producer_1.screen_name.screen_name]
+    Consumer.create 'follow', consumer.screen_name, producers.first.screen_name
+    Consumer.create 'follow', consumer.screen_name, producers.last.screen_name
+
+    # === test it
+    list = Message.read_list 'follow', consumer
+    list.map { |m| m.data.body }.should == ['Msg 1']
+  end
+
+  it "does not retreive messages if consumer is banned by producer" do
+    consumer = customer_create 'bob'
+
+    producer_1 = customer_create "star_1"
+    Message.create producer_1.screen_name, "Prod 1"
+
+    producer_2 = customer_create "star_2"
+    Message.create producer_1.screen_name, "Prod 2"
+
+    Consume.create 'follow', consumer.screen_name, producer_1.screen_name
+    Consume.create 'follow', consumer.screen_name, producer_2.screen_name
+    Consume.create 'ban',    consumer.screen_name, producer_2.screen_name
+
+    # === test it
+    list = Message.read_list 'follow', consumer
+    list.map { |m| m.data.body }.should == ["Prod _1"]
+  end
+
 
 end # === desc
