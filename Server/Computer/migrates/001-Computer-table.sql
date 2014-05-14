402ade20cf8f2274b49dba8638ba964ef3c06893
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-14 15:50:30 -0500

    Updated: Computer table definition

--- Server/Computer/migrates/001-Computer-table.sql
+++ Server/Computer/migrates/001-Computer-table.sql
@@ -1,23 +1,27 @@
 
 CREATE TABLE computer (
 
-  id             SERIAL                                    PRIMARY KEY,
+  id                SERIAL                PRIMARY KEY,
 
   class_id          smallint              NOT NULL,
 
-  producer_id       integer               NOT NULL,
-  producer_class_id smallint              NOT NULL,
+  parent_id         integer               NOT NULL,
+  parent_class_id   smallint              NOT NULL,
 
-  consumer_id       integer               NOT NULL,
-  consumer_class_id smallint              NOT NULL,
+  author_id         integer               NOT NULL,
+  code_author_id    integer               NOT NULL,
+
+  file_name         varchar(15)           NOT NULL,
+  title             varchar(60)           NOT NULL,
 
   code           text                     DEFAULT NULL,
   ss_code        text                     DEFAULT NULL,
-  created_at     timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
+
+  created_at     timestamp with time zone NOT NULL    DEFAULT timezone('UTC'::text, now()),
   updated_at     timestamp with time zone,
 
   CONSTRAINT "computer_unique_idx"
-    UNIQUE (producer_id, producer_class_id, class_id)
+    UNIQUE (parent_id, parent_class_id, file_name)
 
 );
 
