9b23e01ac80bcdb5205a87f7140785cb1a2a11c3
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-14 15:53:29 -0500

    Added: File_Name model and migration.

---
+++ Server/File_Name/migrates/001-File_Name-table.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE file_name (
+
+  id SERIAL PRIMARY KEY,
+  file_name varchar(15) NOT NULL
+
+  CONSTRAINT "file_name_unique_idx"
+    UNIQUE (file_name)
+);
+
+
+
+-- DOWN
+
+DROP TABLE IF EXISTS file_name CASCADE;
+
+
