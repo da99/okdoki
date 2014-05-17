20480da716005b2cc62395ff7f85960c235ad7e1
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-17 02:34:21 -0500

    Changed: Computer authors, computer_authors, relationships moved to Consume

--- Server/Computer/migrates/001-Computer-table.sql
+++ Server/Computer/migrates/001-Computer-table.sql
@@ -3,25 +3,18 @@ CREATE TABLE computer (
 
   id                SERIAL                PRIMARY KEY,
 
-  -- class_id refers to a file_name id
-  class_id          smallint              NOT NULL,
-
   -- generated from:
   --   1 + MAX(file_id OF parent_id, parent_class_id, class_id)
   file_id           smallint              NOT NULL,
 
-  parent_id         integer               NOT NULL,
-  parent_class_id   smallint              NOT NULL,
-
-  author_id          integer              NOT NULL,
-  computer_author_id integer              NOT NULL,
+  owner_id          integer               NOT NULL,
 
   title             varchar(60)           NOT NULL,
 
   code              text                  DEFAULT NULL,
   ss_code           text                  DEFAULT NULL,
 
-  created_at     timestamp with time zone NOT NULL    DEFAULT timezone('UTC'::text, now()),
+  created_at     timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
   updated_at     timestamp with time zone,
 
   CONSTRAINT "computer_unique_idx"
