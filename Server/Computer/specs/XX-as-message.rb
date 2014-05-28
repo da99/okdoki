cc94809dedb220a8e7b703573b900a06925d888e
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-27 21:37:32 -0500

    Changed: Chit_Chat -> Computer

---
+++ Server/Computer/specs/XX-as-message.rb
@@ -0,0 +1,17 @@
+
+require './Server/Computer/model'
+
+describe "Computer: create as message" do
+
+  it "deletes old messages if message limit reached"
+  it "raises Invalid if body is empty"
+  it "raises Invalid if too any chars"
+
+end # === describe create ===
+
+describe "Computer: read as message" do
+
+  it "reads comment count for each message"
+
+end # === describe read ===
+
