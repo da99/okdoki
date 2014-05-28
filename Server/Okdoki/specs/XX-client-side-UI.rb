98a15ee77bc9b185d516964ab47f8dd74a973d02
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-27 23:31:41 -0500

    Changed: UI -> Okdoki spec client-side-UI

---
+++ Server/Okdoki/specs/XX-client-side-UI.rb
@@ -0,0 +1,20 @@
+
+require './Server/Okdoki/model'
+
+describe "Okdoki: client-side-UI" do
+
+  describe "Logging in" do
+
+    it "shows err msg: Too many bad log-ins for today. Try again tomorrow."
+
+    it "removes 'Create acccount' after too many failed log-ins"
+
+    it "after refresh, 'Create account' does not appear if too many failed log-ins"
+
+    it "tells you if screen name is already taken"
+
+  end # === desc
+
+end # === describe Okdoki: client-side-UI ===
+
+
